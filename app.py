e6fa676fa7303aa157608310921ecac32c9cb37b
Emerson Jacobson <emwjacobson@gmail.com>
2020-07-09 09:46:54 -0700

    Started work on basic Flask app.

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Hello World!"
+
+
+@app.route("/test")
+def test():
+    return "This is a test page!"
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return "Page not found."
